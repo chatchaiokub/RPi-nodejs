6d1759f0f17e010ed54f3db56ca0819621da83e4
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro.local>
2016-11-09 15:25:53 +0700

    design

--- models/snap/snap.route.js
+++ models/snap/snap.route.js
@@ -2,13 +2,13 @@
   'use strict'
   var express = require('express')
   var PythonShell = require('python-shell')
-  var router  = express.Router()
+  var router = express.Router()
 
   router.get('/click', function (req, res, next) {
     PythonShell.run('snapshot.py', function (err) {
       if (err) { res.send(err) }
-      else res.send({message : 'done'});
-    });
-  });
+      else res.send({message: 'done'})
+    })
+  })
   module.exports = router
 })()
