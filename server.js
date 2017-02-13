464307cb08a48d620570b4e320bc3909f92b9e2a
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro.local>
2017-02-13 23:04:48 +0700

    fix

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var db = require('./models/db/db.route.js')
 var freeze = require('./models/freeze/freeze.route.js')
 var app = express()
 
- mongoose.connect('mongodb://fridge:fridge@ds133348.mlab.com:33348/smart-fridge')
-//mongoose.connect('mongodb://localhost/smart-fridge')
+// mongoose.connect('mongodb://fridge:fridge@ds133348.mlab.com:33348/smart-fridge')
+mongoose.connect('mongodb://localhost/smart-fridge')
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
